729d0bf20d45bbad59e39033e318ad540efc6186
Krunal Sawarkar <113889481+Krunalsawarkar@users.noreply.github.com>
2024-06-23 16:09:35 +0000

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import Tasks from "@/components/ui/todo/tasks";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>whatTodo</h1>
       <Button>Submit</Button>
+      <Tasks />
     </main>
   );
 }
